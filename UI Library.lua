0dcb79034d4e31581eed9bdd237fca3d4f1baa87
Y3M <64749656+Y3M@users.noreply.github.com>
2020-06-06 17:56:17 -0400

    Update UI Library.lua

--- UI Library.lua
+++ UI Library.lua
@@ -1,84 +1,100 @@
-local library = {}
-function library:Window(name)
-local UI = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
-local ScrollingFrame = Instance.new("ScrollingFrame")
-local UIListLayout = Instance.new("UIListLayout")
-local help = Instance.new("TextLabel")
+local Main = {}
+
+function Main:AddFrame(Textname)
+local Library = Instance.new("ScreenGui")
+local UI = Instance.new("ImageLabel")
+local FrameText = Instance.new("TextLabel")
+local Frame = Instance.new("ImageLabel")
+local PlaceItemsHere = Instance.new("ScrollingFrame")
+local List_Manager = Instance.new("UIListLayout")
+
+Library.Name = "Library"
+Library.Parent = game.Players.LocalPlayer:WaitForChild("StarterGui")
 
 UI.Name = "UI"
-UI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+UI.Parent = Library
+UI.Active = true
+UI.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+UI.BackgroundTransparency = 1.000
+UI.Position = UDim2.new(0.0544747077, 0, 0.0617529899, 0)
+UI.Size = UDim2.new(0, 202, 0, 44)
+UI.Image = "rbxassetid://3570695787"
+UI.ImageColor3 = Color3.fromRGB(40, 40, 40)
+UI.ScaleType = Enum.ScaleType.Slice
+UI.SliceCenter = Rect.new(100, 100, 100, 100)
+UI.SliceScale = 0.120
 
+FrameText.Name = "FrameText"
+FrameText.Parent = UI
+FrameText.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+FrameText.BackgroundTransparency = 1.000
+FrameText.BorderColor3 = Color3.fromRGB(40, 40, 40)
+FrameText.BorderSizePixel = 0
+FrameText.Size = UDim2.new(0, 202, 0, 44)
+FrameText.Font = Enum.Font.GothamSemibold
+FrameText.Text = Textname
+FrameText.TextColor3 = Color3.fromRGB(255, 255, 255)
+FrameText.TextSize = 23.000
+FrameText.TextWrapped = true
+
+Frame.Name = "Frame"
 Frame.Parent = UI
-Frame.Active = true
-Frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-Frame.BorderColor3 = Color3.fromRGB(187, 28, 198)
-Frame.BorderSizePixel = 2
-Frame.Position = UDim2.new(0.0334608033, 0, 0.143426299, 0)
-Frame.Size = UDim2.new(0, 165, 0, 29)
-Frame.Draggable = true
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0, 0, 1.13636363, 0)
+Frame.Size = UDim2.new(0, 202, 0, 288)
+Frame.Image = "rbxassetid://3570695787"
+Frame.ImageColor3 = Color3.fromRGB(40, 40, 40)
+Frame.ScaleType = Enum.ScaleType.Slice
+Frame.SliceCenter = Rect.new(100, 100, 100, 100)
+Frame.SliceScale = 0.120
 
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(187, 28, 198)
-TextLabel.BorderSizePixel = 0
-TextLabel.Size = UDim2.new(0, 165, 0, 29)
-TextLabel.Font = Enum.Font.Cartoon
-TextLabel.Text = name
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14.000
-TextLabel.TextWrapped = true
+PlaceItemsHere.Name = "PlaceItemsHere"
+PlaceItemsHere.Parent = Frame
+PlaceItemsHere.Active = true
+PlaceItemsHere.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+PlaceItemsHere.BorderSizePixel = 0
+PlaceItemsHere.Position = UDim2.new(0.0396039598, 0, 0.024305556, 0)
+PlaceItemsHere.Size = UDim2.new(0, 186, 0, 273)
 
-ScrollingFrame.Parent = Frame
-ScrollingFrame.Active = true
-ScrollingFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-ScrollingFrame.BorderColor3 = Color3.fromRGB(187, 28, 198)
-ScrollingFrame.BorderSizePixel = 2
-ScrollingFrame.Position = UDim2.new(0, 0, 1.20689654, 0)
-ScrollingFrame.Size = UDim2.new(0, 165, 0, 241)
+List_Manager.Name = "List_Manager"
+List_Manager.Parent = PlaceItemsHere
+List_Manager.HorizontalAlignment = Enum.HorizontalAlignment.Center
+List_Manager.SortOrder = Enum.SortOrder.LayoutOrder
 
-UIListLayout.Parent = ScrollingFrame
-UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+local Items = {}
 
-help.Name = "help"
-help.Parent = Frame
-help.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-help.Position = UDim2.new(1.15151513, 0, 1, 0)
-help.Size = UDim2.new(0, 656, 0, 297)
-help.Font = Enum.Font.SourceSans
-help.Text = "P to OPEN/CLOSE"
-help.TextColor3 = Color3.fromRGB(0, 0, 0)
-help.TextScaled = true
-help.TextSize = 14.000
-help.TextWrapped = true
-
-local epic = {}
-
-function epic:Button(buttontext, callback)
-callback = callback or function() end
+function Items:AddButton(buttonText, getscript)
+-- [[ ]] --
+getscript = getscript or function() end
+-- [[ ]] --
+local ButtonFrame = Instance.new("ImageLabel")
 local TextButton = Instance.new("TextButton")
-TextButton.Text = buttontext
-TextButton.Parent = ScrollingFrame
+ButtonFrame.Name = "ButtonFrame"
+ButtonFrame.Parent = PlaceItemsHere
+ButtonFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+ButtonFrame.BackgroundTransparency = 1.000
+ButtonFrame.Position = UDim2.new(0.112903222, 0, 0.0171512514, 0)
+ButtonFrame.Size = UDim2.new(0, 144, 0, 31)
+ButtonFrame.Image = "rbxassetid://3570695787"
+ButtonFrame.ImageColor3 = Color3.fromRGB(30, 30, 30)
+ButtonFrame.ScaleType = Enum.ScaleType.Slice
+ButtonFrame.SliceCenter = Rect.new(100, 100, 100, 100)
+ButtonFrame.SliceScale = 0.120
+TextButton.Parent = ButtonFrame
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.BackgroundTransparency = 1.000
-TextButton.BorderSizePixel = 0
-TextButton.Size = UDim2.new(0, 165, 0, 29)
-TextButton.Font = Enum.Font.SciFi
+TextButton.Text = buttonText
+TextButton.Position = UDim2.new(0.0486111119, 0, 0.0967741907, 0)
+TextButton.Size = UDim2.new(0, 130, 0, 25)
+TextButton.Font = Enum.Font.GothamSemibold
 TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.TextScaled = true
-TextButton.TextSize = 14.000
+TextButton.TextSize = 23.000
 TextButton.TextWrapped = true
 TextButton.MouseButton1Click:Connect(function()
-	pcall(callback)
+pcall(getscript)
 end)
 end
-
-
-wait(2)
-help:Destroy()
-return epic
+return Items
 end
-return library
+return Main
